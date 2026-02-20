7cbd15ff7ca8fbd3b1054335a2a71050b91cc418
keyur-tbd <keyur@thebakersdozen.in>
2026-02-20 20:05:17 +0530

    Change START_DATE to 3 days ago

--- meta_region.py
+++ meta_region.py
@@ -20,7 +20,7 @@ AD_ACCOUNT_IDS = [
 ]
 
 END_DATE   = datetime.today().strftime("%Y-%m-%d")
-START_DATE = (datetime.today() - timedelta(days=30)).strftime("%Y-%m-%d")
+START_DATE = (datetime.today() - timedelta(days=3)).strftime("%Y-%m-%d")
 
 TABLE_NAME = "meta_ads_region"
 
